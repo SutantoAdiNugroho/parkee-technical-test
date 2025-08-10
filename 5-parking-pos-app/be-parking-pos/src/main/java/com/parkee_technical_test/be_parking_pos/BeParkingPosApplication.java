1b4e2f0a03f1a80d1d65bb8cb24f2aecea9da084
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2025-08-10 16:41:11 +0700

    feat: parking pos starter project for backend and frontend

---
+++ 5-parking-pos-app/be-parking-pos/src/main/java/com/parkee_technical_test/be_parking_pos/BeParkingPosApplication.java
@@ -0,0 +1,13 @@
+package com.parkee_technical_test.be_parking_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeParkingPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeParkingPosApplication.class, args);
+	}
+
+}
---
+++ 5-parking-pos-app/be-parking-pos/src/test/java/com/parkee_technical_test/be_parking_pos/BeParkingPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkee_technical_test.be_parking_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeParkingPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
